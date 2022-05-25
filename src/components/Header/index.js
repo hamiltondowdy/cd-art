bca8618debac3602fa76eb4990ab461a09d2cd16
hamiltondowdy <hddowdy@ncsu.edu>
2022-05-24 23:57:16 -0500

    updated responsiveness

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,6 +8,11 @@ function Header () {
             <div id="logo">
                 <h1>Christina Dowdy</h1>
                 <h4>Original Oil Paintings</h4>
+                <div id="oop">
+
+                
+                </div>
+                
             </div>
             <div class="container red topBotomBordersOut">
   <a href="/">Home</a>
