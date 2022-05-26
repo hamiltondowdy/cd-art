7448aa56b05619fa6198138b7df53a8b99321403
hamiltondowdy <hddowdy@ncsu.edu>
2022-05-26 16:19:39 -0500

    refactor and final touches to shading

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 function Socials () {
     return (
         <div class="footer-social-links">
-  <a href="#" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-  <a href="#" title="Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a>
-  <a href="#" title="Instagram+" target="_blank"><i class="fa fa-instagram"></i></a>
-  <a href="#" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin"></i></a>
-  <a href="#" title="Dribbble" target="_blank"><i class="fa fa-envelope"></i></a>
+  <a href="https://www.facebook.com/ChristinaDowdyART" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+  <a href="https://www.instagram.com/christinadowdyart/" title="Pinterest" target="_blank"><i class="fa fa-pinterest"></i></a>
+  <a href="https://www.instagram.com/christinadowdyart/" title="Instagram+" target="_blank"><i class="fa fa-instagram"></i></a>
+  <a href="https://www.instagram.com/christinadowdyart/" title="LinkedIn+" target="_blank"><i class="fa fa-linkedin"></i></a>
+  <a href="mailto:crwdowdy@gmail.com" title="Email" target="_blank"><i class="fa fa-envelope"></i></a>
 </div>
         
     )
